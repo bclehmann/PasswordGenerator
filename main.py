b585e2bf535a9ea5cd51ddbeee87ad20627933b9
Benjamin Lehmann <bclehmann@yahoo.ca>
2022-01-29 18:30:57 -0700

    Dedup charset

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser.add_argument('--alphanumeric', default=False, action='store_true')
 
     args = parser.parse_args()
-    charset = ALPHANUMERIC_CHARSET if args.alphanumeric else args.charset
+    charset = list(set(ALPHANUMERIC_CHARSET if args.alphanumeric else args.charset))
 
     cryptoRandom = random.SystemRandom()
     print(''.join(map(lambda _: cryptoRandom.choice(charset), range(args.length))))
